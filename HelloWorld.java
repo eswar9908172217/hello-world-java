68cab6fdac7176bf3259881df3c7c94d3a9cca44
eswar <pg.eswarkumar@gmail.com>
2022-02-20 14:09:26 +0000

    Changed code

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,10 +34,10 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
-    System.out.println("this is madhu");
-    System.out.println("Hello, Banglore");
-    System.out.println("Hello, canada");
+    System.out.println("Hello, eswar");
+    System.out.println("this is nicy");
+    System.out.println("Hello, suma");
+    System.out.println("Hello, esware");
     System.out.println("Hello, kadapa ");
     System.out.println("Hello, wed......................");
   }
